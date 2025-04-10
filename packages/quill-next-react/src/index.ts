be5d9d0bd93f011d022782f52a0ea96eb488e2f5
Vincent Chan <okcdz@diverse.space>
2025-04-10 21:24:35 +0800

    feat: Add Parchment dependency and ForkedRegistry class

--- packages/quill-next-react/src/index.ts
+++ packages/quill-next-react/src/index.ts
@@ -1,9 +1,11 @@
 import { QuillContext } from "./context/quill-context";
 import { QuillEditor, type EditorChangeHandler, type IQuillEditorProps } from "./editor.component";
+import { ForkedRegistry } from "./forked-registry";
 
 export {
   QuillEditor as default,
   QuillContext,
+  ForkedRegistry,
   type EditorChangeHandler,
   type IQuillEditorProps
 };
--- packages/quill-next-react/vite.config.ts
+++ packages/quill-next-react/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         'react',
         'react/jsx-runtime',
         'quill-next',
+        'parchment',
       ],
     },
   },
