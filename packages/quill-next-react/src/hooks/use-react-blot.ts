7f5f94035e1d653905566f40e4cf9fabdda53a95
Vincent Chan <okcdz@diverse.space>
2025-04-12 14:11:00 +0800

    feat: Update module resolution to "bundler" in tsconfig.json

---
+++ packages/quill-next-react/src/hooks/use-react-blot.ts
@@ -0,0 +1,91 @@
+import React, { useMemo, useRef, useEffect } from "react";
+import { createRoot, Root as ReactRoot } from "react-dom/client";
+import { BlotConstructor, EmbedBlot, Scope, Root } from "parchment";
+
+export type BlotScope = 'block' | 'inline';
+
+export interface IRenderOptions {
+  value: unknown;
+  attributes?: Record<string, unknown>;
+}
+
+export type RenderFunc = (optoins: IRenderOptions) => React.ReactNode;
+
+export interface IReactBlotOptions {
+  blotName: string;
+  scope?: BlotScope;
+  tagName?: string;
+  create?: (value?: unknown) => HTMLElement;
+  render: RenderFunc;
+}
+
+export function useEmbedBlot(
+  options: IReactBlotOptions,
+): BlotConstructor {
+  const { blotName, tagName, create, render, scope } = options;
+
+  const renderFuncRef = useRef<RenderFunc>(render);
+  useEffect(() => {
+    renderFuncRef.current = render;
+  }, [render]);
+
+  return useMemo(() => {
+    let blotScope = Scope.INLINE;
+    if (scope === 'block') {
+      blotScope = Scope.BLOCK;
+    }
+
+    return class extends EmbedBlot {
+      static override blotName: string = blotName;
+      static override tagName = tagName || EmbedBlot.tagName;
+      static override scope: Scope = blotScope;
+
+      static override create(value?: unknown) {
+        if (create) {
+          return create(value) as HTMLElement;
+        }
+        return super.create(value) as HTMLElement;
+      }
+
+      private root: ReactRoot | null;
+      #value: unknown = undefined;
+      #attributes: Record<string, unknown> = {};
+
+      constructor(scroll: Root, domNode: Node, value?: unknown) {
+        super(scroll, domNode);
+        if (value) {
+          this.#value = value;
+        }
+      }
+
+      override attach(): void {
+        this.root = createRoot(this.domNode as HTMLElement);
+      }
+
+      override detach(): void {
+        if (this.root) {
+          this.root.unmount();
+          this.root = null;
+        }
+      }
+
+      override value(): unknown {
+        return {
+          [blotName]: this.#value,
+        }
+      }
+
+      override format(name: string, value: string): void {
+        this.#attributes[name] = value;
+        this.render();
+      }
+
+      render() {
+        this.root.render(renderFuncRef.current({
+          value: this.#value,
+          attributes: this.#attributes,
+        }))
+      }
+    }
+  }, [scope, blotName, tagName, create]);
+}
--- packages/quill-next-react/src/index.ts
+++ packages/quill-next-react/src/index.ts
@@ -1,11 +1,21 @@
 import { QuillContext } from "./context/quill-context";
 import { QuillEditor, type EditorChangeHandler, type IQuillEditorProps } from "./editor.component";
 import { ForkedRegistry } from "./forked-registry";
+import {
+  useEmbedBlot,
+  type BlotScope,
+  type IRenderOptions,
+  type IReactBlotOptions,
+} from "./hooks/use-react-blot";
 
 export {
   QuillEditor as default,
   QuillContext,
   ForkedRegistry,
+  useEmbedBlot,
   type EditorChangeHandler,
-  type IQuillEditorProps
+  type IQuillEditorProps,
+  type BlotScope,
+  type IRenderOptions,
+  type IReactBlotOptions,
 };
