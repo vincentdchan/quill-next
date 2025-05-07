786ce1168b89e85783addd3e23883baec89576b2
Vincent Chan <okcdz@diverse.space>
2025-05-07 23:41:37 +0800

    docs: Update QuillEditor example and enhance configuration documentation
    - Simplified the QuillEditor example by removing unnecessary wrapper div.
    - Added a new SandpackWithReact example demonstrating the use of custom blots and Notion-like toolbars in QuillEditor.
    - Expanded the configuration documentation to include detailed usage of the new example, improving clarity for users integrating Quill with React.

--- packages/quill-next-react/src/notion-like/plugins/notion-link-toolbar.plugin.tsx
+++ packages/quill-next-react/src/notion-like/plugins/notion-link-toolbar.plugin.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NotionLinkToolbar } from "../components/notion-link-toolbar.component";
 import { LinkToolbarPlugin } from "../../plugins/link-toolbar-plugin";
 
