89f3c2c8e92e3a084a98d9f3637421a935df175f
Vincent Chan <okcdz@diverse.space>
2025-05-09 11:35:30 +0800

    docs: Revise Quill Next description for clarity and modernize content
    - Updated the description of Quill Next to emphasize its role as a modern rich text editor built on Quill.
    - Added context about the project's personal nature and its goal to keep Quill evolving.
    - Removed redundant empty paragraphs to streamline the content presentation.

--- packages/website/src/pages/index.jsx
+++ packages/website/src/pages/index.jsx
@@ -66,8 +66,9 @@ const content = () => {
                 <h1>Quill Next Editor</h1>
                 <p><br></p>
                 <p>
-                  Quill Next is a fork of <a href="https://github.com/slab/quill/">Quill</a>.
-                  Created by <span class="qn-mention" data-value="Vincent Chan">Vincent Chan</span>
+                  <b>Quill Next</b> is a modern rich text editor built on the foundation of <a href="https://github.com/slab/quill/">Quill</a>.
+                  Created by <span class="qn-mention" data-value="Vincent Chan">Vincent Chan</span>.
+                  This fork is currently a personal project, aiming to keep Quill thriving and evolving.
                 </p>
                 <p><br></p>
                 <iframe class="ql-video ql-align-center" src="https://player.vimeo.com/video/253905163" width="500" height="280" allowfullscreen></iframe>
@@ -87,11 +88,6 @@ const content = () => {
 <span class="hljs-comment">// Open your browser's developer console to try out the API!</span>
 </pre>
                 <p><br></p>
-                <p><br></p>
-                <p class="ql-align-center"><strong>Built with</strong></p>
-                <p class="ql-align-center"><span class="ql-formula" data-value="x^2 + (y - \\sqrt[3]{x^2})^2 = 1"></span></p>
-                <p><br></p>
-
 `;
 };
 
