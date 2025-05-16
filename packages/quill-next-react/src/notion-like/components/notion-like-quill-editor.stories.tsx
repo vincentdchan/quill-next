1f100adf63f42f67f094398a26b7ca9a9f8ff5c5
Vincent Chan <okcdz@diverse.space>
2025-05-16 16:23:50 +0800

    feat: Add Notion-like Quill editor stories and enhance styling
    - Introduced new Storybook stories for the NotionLikeQuillEditor component, showcasing basic and dark mode configurations.
    - Enhanced editor styles by adding specific styles for code blocks and ordered lists, improving visual presentation and user experience.
    - Defined a new mono font family for code formatting to ensure consistent typography across the editor.

--- packages/quill-next-react/src/notion-like/components/notion-like-quill-editor.stories.tsx
+++ packages/quill-next-react/src/notion-like/components/notion-like-quill-editor.stories.tsx
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { NotionLikeQuillEditor } from "./notion-like-quill-editor";
 import { Delta } from "quill-next";
+import React from 'react';
 
-const meta = {
+const meta: Meta<typeof NotionLikeQuillEditor> = {
   title: "QuillEditor/NotionLike",
   component: NotionLikeQuillEditor,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
@@ -11,7 +12,7 @@ const meta = {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: "fullscreen",
   },
-} satisfies Meta<typeof NotionLikeQuillEditor>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -30,6 +31,38 @@ export const Basic: Story = {
           naturalHeight: 197,
         },
       })
+      .insert("\n")
+      .insert("Hello World! Select some text to see the toolbar.\n")
+      .insert("Code", { code: true })
+      .insert("\n")
+      .insert("Hello World! Select some text to see the toolbar."),
+  },
+};
+
+export const DarkMode: Story = {
+  decorators: [
+    (Story) => {
+      React.useEffect(() => {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      }, []);
+      return <Story />;
+    },
+  ],
+  args: {
+    defaultValue: new Delta()
+      .insert("Heading 1\n", { header: 1 })
+      .insert("Heading 2\n", { header: 2 })
+      .insert("Heading 3\n", { header: 3 })
+      .insert({
+        image:
+          "https://github.com/vincentdchan/quill-next/raw/main/images/quill-next.png",
+        attributes: {
+          naturalWidth: 800,
+          naturalHeight: 197,
+        },
+      })
+      .insert("\n")
+      .insert("Code", { code: true })
       .insert("\n")
       .insert("Hello World! Select some text to see the toolbar."),
   },
--- packages/quill-next-react/src/notion-like/components/notion-like-quill-editor.style.ts
+++ packages/quill-next-react/src/notion-like/components/notion-like-quill-editor.style.ts
@@ -1,10 +1,24 @@
 import { css } from "@emotion/react";
-import { defaultFontFamily } from "../constants/font-family";
+import { defaultFontFamily, monoFontFamily } from "../constants/font-family";
 
 export const notionLikeQuillEditor = css({
   ".ql-editor": {
     fontFamily: defaultFontFamily,
     fontSize: "16px",
     lineHeight: 1.5,
+
+    "code": {
+      lineHeight: 1.5,
+      color: "#EB5757",
+      fontFamily: monoFontFamily,
+      background: "rgba(135, 131, 120, .15)",
+      borderRadius: "4px",
+      fontSize: "85%",
+      padding: "0.2em 0.4em",
+    },
+
+    "ol": {
+      paddingLeft: "0px",
+    }
   },
 });
